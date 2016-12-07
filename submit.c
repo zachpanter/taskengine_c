5131d8cbd2e082d18427ba66ed92b215b5abb6bc
Zach Panter <zachpanter@gmail.com>
2016-12-07 15:09:03 -0500

    Messed stuff up with left_window_ptr

--- submit.c
+++ submit.c
@@ -20,14 +20,17 @@ struct window_struct *window_info_ptr;
 
 // NCURSES GLOBAL VARIABLES
 unsigned int z = 0;
-int counter1 = 1;
-int counter2 = -1;
+//int counter1 = 1;
+//int counter2 = -1;
 void navDiv(struct window_struct);
 void *statusDiv(void *arg);
 pthread_mutex_t ncurses;
 int main_height;
 int half_width;
 int status_height;
+WINDOW *left_window_ptr;
+WINDOW *right_window_ptr;
+WINDOW *status_window_ptr;
 
 // MYSQL GLOBAL VARIABLES
 static char *opt_host_name = NULL; // server host (default = localhost)
@@ -56,6 +59,9 @@ int task_repo_max = 5;
 int string_length = 50;
 int n_choices = 0;
 int my_num_rows = 0;
+void func(char *name);
+ITEM *cur;
+void (*p)(char *);
 
 int main()
 {
@@ -77,18 +83,16 @@ int main()
 
 
     // INITIALIZE NCURSES & WINDOWS
-	WINDOW *left_window_ptr;
-	WINDOW *right_window_ptr;
-	WINDOW *status_window_ptr;
+
 
 	initscr(); // initialize ncurses
 	cbreak();
 	timeout(1);
 	refresh();
 	noecho();
-	main_height = LINES - 1;
+	main_height = LINES - 3;
 	half_width = COLS/2;
-	status_height = 2;
+	status_height = 3;
 	left_window_ptr = newwin(main_height, half_width,0,0);
 	right_window_ptr = newwin(main_height, half_width,0,half_width);
 	status_window_ptr = newwin(status_height,COLS,main_height,0); 
@@ -142,6 +146,8 @@ int main()
 	}
 
 	//sleep(3);
+	unpost_menu(my_menu);
+	free_menu(my_menu);
 	free(my_items);
 	nocbreak();	
 	echo();
@@ -159,7 +165,7 @@ void navDiv(struct window_struct window_info)
 	right_window_ptr = window_info.window_two;
 
 	// INITIAL DISPLAY
-		//pthread_mutex_lock(&ncurses); 	// Use pthread_mutex_lock() and pthread_mutex_unlock() to create the critical sections
+		pthread_mutex_lock(&ncurses); 	// Use pthread_mutex_lock() and pthread_mutex_unlock() to create the critical sections
 		wrefresh(left_window_ptr);
 		wrefresh(right_window_ptr);
 		keypad(left_window_ptr, TRUE);
@@ -186,6 +192,7 @@ void navDiv(struct window_struct window_info)
 			// wprintw(left_window_ptr," %s\n",row[0]);
 			//strcpy(repo_array[k],row[0]);
 			my_items[k] = new_item(row[0]," ");
+			// set_item_userptr(my_items[k], func);
 			++k;
 		}
 		//strcpy(repo_array[k],(char *)NULL); // zero termination
@@ -211,8 +218,10 @@ void navDiv(struct window_struct window_info)
 		// Draw BorderS
 		wborder(left_window_ptr, '|','|','-','-','*','*','*','*'); //box(arg,'*','*');
 		wborder(right_window_ptr, '|','|','-','-','*','*','*','*'); //box(arg,'*','*');
+		wborder(status_window_ptr, '|','|','-','-','*','*','*','*'); //box(arg,'*','*');
 		wrefresh(left_window_ptr);
 		wrefresh(right_window_ptr);
+		wrefresh(status_window_ptr);
 		//pthread_mutex_unlock(&ncurses);
 
 	// ACTION HANDLER LOOP
@@ -226,7 +235,7 @@ void navDiv(struct window_struct window_info)
 		// wrefresh(arg);
 		// wattron(arg, COLOR_PAIR(1));
 
-		//pthread_mutex_unlock(&ncurses);
+		pthread_mutex_unlock(&ncurses);
 		switch(c)
 	    {	
 			case KEY_DOWN:
@@ -241,6 +250,15 @@ void navDiv(struct window_struct window_info)
 			case KEY_PPAGE:
 				menu_driver(my_menu, REQ_SCR_UPAGE);
 				break;
+			// case 10: /* ENTER */
+				
+				
+
+			// 	cur = current_item(my_menu);
+			// 	p = item_userptr(cur);
+			// 	p((char *)item_name(cur));
+			// 	pos_menu_cursor(my_menu);
+			// 	break;
 		}
 
 		// if(getch() == 'q' || getch() == 'Q') // replace with a handler for the up and down arrows to select a repo
@@ -279,16 +297,17 @@ void navDiv(struct window_struct window_info)
 // TO BE REPLACED WITH THE STATUS BAR WINDOW HANDLER
 void *statusDiv(void *arg)
 {
-	// DIVIDING LINE
 	pthread_mutex_lock(&ncurses);
-	wattron(arg, COLOR_PAIR(2));
-	char blueLine[COLS];
-	strcpy(blueLine," ");
-	for(z = 0; z < COLS-2; z++)
-	{
-		strcat(blueLine," ");
-	}
-	wprintw(arg,"%s",blueLine);
+	// wattron(arg, COLOR_PAIR(2));
+	// char blueLine[COLS];
+	// strcpy(blueLine," ");
+	// for(z = 0; z < COLS-2; z++)
+	// {
+	// 	strcat(blueLine," ");
+	// }
+	// wprintw(arg,"%s",blueLine);
+
+
 	wrefresh(arg);
 	pthread_mutex_unlock(&ncurses);
 	pthread_exit(NULL);
@@ -367,4 +386,14 @@ process_statement (MYSQL *conn, char *stmt_str, WINDOW *arg)
       wprintw(arg, "Could not retrieve result set");
     }
   }
+}
+
+void func(char *name)
+{	//move(20, 0);
+	//wclrtoeol(status_window_ptr);
+	//mvprintw(20, 0, "Item selected is : %s", name);
+	pthread_mutex_lock(&ncurses);
+	wprintw(status_window_ptr,"Item selected is : %s\n", name);
+	wrefresh(status_window_ptr);
+	pthread_mutex_unlock(&ncurses);
 }
